aad37a466fb36e4dd555be11b240f6b35b4ace3c
tanay <ashwin.td@gmail.com>
2017-12-02 13:25:46 -0500

    vector

--- HackPHS_CTF/src/Vector.java
+++ HackPHS_CTF/src/Vector.java
@@ -3,13 +3,13 @@ public class Vector {
 	
 	private double x;
 	private double y;
-	private double speed;
+	//private double speed;
 	
-	public Vector(double x, double y, double speed)
+	public Vector(double x, double y)
 	{
 		this.x = x;
 		this.y = y;
-		this.speed = speed;
+		//this.speed = speed;
 	}
 	
 	public double getX()
@@ -22,8 +22,32 @@ public class Vector {
 		return y;
 	}
 	
-	public double getSpeed()
+	/*public double getSpeed()
 	{
 		return speed;
+	}*/
+	
+	public double dotProd(Vector other) {
+		return this.x * other.x + this.y * other.y;
+	}
+	
+	public Vector plus(Vector other) {
+		return new Vector(this.x + other.x, this.y + other.y);
+	}
+	
+	public Vector minus(Vector other) {
+		return new Vector(this.x - other.x, this.y - other.y);
+	}
+	
+	public Vector times(double k) {
+		return new Vector(k*this.x, k*this.y);
+	}
+	
+	public double mag() {
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	}
+	
+	public double angle() {
+		return Math.atan(this.y / this.x);
 	}
 }
